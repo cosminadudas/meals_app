0485eec3704aef7857ed9a69a3a3d3ab0c3d9ed7
cosminadudas <cosminadudas@users.noreply.github.com>
2021-05-08 22:30:45 +0300

    refactors page navigation using named routes

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
 
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -29,7 +30,12 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
