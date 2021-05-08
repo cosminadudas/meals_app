e0805be21788c3596cc70bb9541fae0d038e4b0a
cosminadudas <cosminadudas@users.noreply.github.com>
2021-05-08 22:14:27 +0300

    adds the homepage of the app as a grid view with meals categories and also adds functionality to tap on a category and go to a different page

---
+++ meals_app/lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1', 
+    title: 'Italian', 
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c2', 
+    title: 'Quick & Easy', 
+    color: Colors.deepOrange,
+  ),
+  Category(
+    id: 'c3', 
+    title: 'Hamburgers', 
+    color: Colors.deepPurple,
+  ),
+  Category(
+    id: 'c4', 
+    title: 'German', 
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c5', 
+    title: 'Light & Lovely', 
+    color: Colors.pinkAccent,
+  ),
+  Category(
+    id: 'c6', 
+    title: 'Exotic', 
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c7', 
+    title: 'Breakfast', 
+    color: Colors.blueAccent,
+  ),
+  Category(
+    id: 'c8', 
+    title: 'Asian', 
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9', 
+    title: 'French', 
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c10', 
+    title: 'Summer', 
+    color: Colors.red,
+  ),
+];
